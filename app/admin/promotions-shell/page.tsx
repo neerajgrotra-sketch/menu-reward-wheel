7a11b77bd73c1b2e4f3124b586f166f5c1b26fd3
neerajgrotra-sketch <neerajgrotra@gmail.com>
2026-05-09 21:51:10 -0400

    Add promotions shell route

---
+++ app/admin/promotions-shell/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { PromotionsAdminPageShell } from '@/components/promotion-builder/PromotionsAdminPageShell';
+
+export default function PromotionsShellPage() {
+  return <PromotionsAdminPageShell />;
+}
